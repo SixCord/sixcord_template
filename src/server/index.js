ec6ee588731b81c16824811838301b155482aa83
v1 <76124818+v1devv@users.noreply.github.com>
2021-01-11 19:55:33 +0100

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const port = 8080
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+})
 
 app.use(express.static('src'));
 
