adfe54faf8623177528ee2d1bc6a4c28b6308aee
v1 <76124818+v1devv@users.noreply.github.com>
2021-01-11 18:02:32 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('src'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/src/app/index.html')
+});
+app.get('/premium', function(req, res) {
+    res.redirect('/')
+});
+app.get('/support', function(req, res) {
+    res.sendFile(__dirname + '/src/app/pages/support.html')
+});
+app.get('/invite', function(req, res) {
+    res.sendFile(__dirname + '/src/app/pages/invite.html')
+});
+app.get('/status', function(req, res) {
+    res.redirect('/')
+});
+app.get('/privacy', function(req, res) {
+    res.sendFile(__dirname + '/src/app/pages/privacy.html')
+});
